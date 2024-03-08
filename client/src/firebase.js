2a5c878e308e648e4e673c2427985244bf698936
gulamyazdani <gulamyazdani03@gmail.com>
2024-03-08 12:02:35 +0530

    Implemented Sign in with Google using firebase

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_KEY,
-  authDomain: "video-7801c.firebaseapp.com",
-  projectId: "video-7801c",
-  storageBucket: "video-7801c.appspot.com",
-  messagingSenderId: "130073115219",
-  appId: "1:130073115219:web:365035e1474abbe7fba4a6",
+  apiKey: "AIzaSyAn18lTzN5qKZGL21_XvxtB6V_nk3klC9Y",
+  authDomain: "clone-e5ab4.firebaseapp.com",
+  projectId: "clone-e5ab4",
+  storageBucket: "clone-e5ab4.appspot.com",
+  messagingSenderId: "845386204107",
+  appId: "1:845386204107:web:6a197af199eeb82a506c4e"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth();
 export const provider = new GoogleAuthProvider();
 
